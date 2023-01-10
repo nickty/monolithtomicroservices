ebee8ccacfe1e70170ec19c4c4efbfd18c2a73cb
Mizanur Rahman <nickty.86@gmail.com>
2023-01-10 08:05:03 +0600

    product service updated

--- products/src/api/products.js
+++ products/src/api/products.js
@@ -81,28 +81,39 @@ module.exports = (app) => {
     const productId = req.params.id;
 
     try {
-      const product = await service.GetProductById(productId);
-      const wishlist = await customerService.AddToWishlist(_id, product);
-      return res.status(200).json(wishlist);
+      const { data } = await service.GetProductPayload(
+        _id,
+        { productId },
+        "REMOVE_FROM_WISHLIST"
+      );
+
+      PublishCustomerEvent(data);
+
+      return res.status(200).json(data.data.product);
     } catch (err) {
       next(err);
     }
   });
 
   app.put("/cart", UserAuth, async (req, res, next) => {
-    const { _id, qty } = req.body;
+    const { _id } = req.user;
 
     try {
-      const product = await service.GetProductById(_id);
-
-      const result = await customerService.ManageCart(
-        req.user._id,
-        product,
-        qty,
-        false
+      const { data } = await service.GetProductPayload(
+        _id,
+        { productId: req.body.id, qty: req.body.qty },
+        "ADD_TO_CART"
       );
 
-      return res.status(200).json(result);
+      PublishCustomerEvent(data);
+      PublishShoppingEvent(data);
+
+      const response = {
+        product: data.data.product,
+        unit: data.data.qty,
+      };
+
+      return res.status(200).json(response);
     } catch (err) {
       next(err);
     }
@@ -110,11 +121,24 @@ module.exports = (app) => {
 
   app.delete("/cart/:id", UserAuth, async (req, res, next) => {
     const { _id } = req.user;
+    const productId = req.params.id;
 
     try {
-      const product = await service.GetProductById(req.params.id);
-      const result = await customerService.ManageCart(_id, product, 0, true);
-      return res.status(200).json(result);
+      const { data } = await service.GetProductPayload(
+        _id,
+        { productId },
+        "REMOVE_FROM_CART"
+      );
+
+      PublishCustomerEvent(data);
+      PublishShoppingEvent(data);
+
+      const response = {
+        product: data.data.product,
+        unit: data.data.qty,
+      };
+
+      return res.status(200).json(response);
     } catch (err) {
       next(err);
     }
