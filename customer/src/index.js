b411b4c9ad77e8bfa667b0e7d0aa6e0723995b3e
Mizanur Rahman <nickty.86@gmail.com>
2023-01-11 08:03:43 +0600

    Communication between product with customer

--- customer/src/index.js
+++ customer/src/index.js
@@ -4,12 +4,16 @@ const { PORT } = require("./config");
 const { databaseConnection } = require("./database");
 const expressApp = require("./express-app");
 
+const { CreateChannel } = require("./utils");
+
 const StartServer = async () => {
   const app = express();
 
   await databaseConnection();
 
-  await expressApp(app);
+  const channel = await CreateChannel();
+
+  await expressApp(app, channel);
 
   app.get("/", (req, res) => {
     res.send("hellowww");
