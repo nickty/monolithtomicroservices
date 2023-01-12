c259735c9cebc01f12e30e3b186281aa598429d5
Mizanur Rahman <nickty.86@gmail.com>
2023-01-12 16:34:27 +0600

    feature one test commit

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,7 @@
 //which service is it
 describe("Customer Service", () => {
-  //which function
+  //which function we are testing
+
   describe("SignIn", () => {
     // which scenario we are testing
     test("validate user inputs", () => {});
