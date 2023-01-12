55fac0da4265444e4df5bfb9561bc7f505227d5a
Mizanur Rahman <nickty.86@gmail.com>
2023-01-12 16:19:31 +0600

    Test cases added

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+//which service is it
+describe("Customer Service", () => {
+  //which function
+  describe("SignIn", () => {
+    // which scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("validate response", () => {});
+  });
+});
