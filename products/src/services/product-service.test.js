cd95adb47ef519ac16b5f389168d5c2bb389480b
Mizanur Rahman <nickty.86@gmail.com>
2023-01-14 18:56:38 +0600

    jest installed

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,11 @@
+//which service is it
+describe("Product Service", () => {
+  //which function we are testing
+
+  describe("SignIn", () => {
+    // which scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("validate response", () => {});
+  });
+});
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,11 @@
+//which service is it
+describe("Shopping Service", () => {
+  //which function we are testing
+
+  describe("SignIn", () => {
+    // which scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("validate response", () => {});
+  });
+});
